c804aae029340a606f5ef5cf33aec2362fef9241
PrinceQuarshie <prince.quarshie@amalitech.org>
2022-10-05 00:21:47 +0000

    Header Completed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {tsParticles} from "tsparticles-engine";
+import particlesConfig from './components/config/particles-config';
 import Navbar from './components/Navbar';
+import Header from './components/Header';
 
 function App() {
   return (
-    <div className="App">
-     <Navbar />
-    </div>
+    <>
+      <tsParticles
+      params={particlesConfig}
+      />
+      <Navbar />
+      <Header />
+    </>
   );
 }
 
